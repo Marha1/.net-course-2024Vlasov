d81d358bd18b77f633ca5bfa823f7d2e6dd2546a
Mar2 <vlasovtimur21@gmail.com>
2024-10-03 18:53:55 +0300

    Fix(Methods have been moved to the repository.     Direct access to the collection has been removed.     Changed tests, calling the storage directly)

--- BankSystem.App/Services/ClientService.cs
+++ BankSystem.App/Services/ClientService.cs
@@ -15,27 +15,6 @@ public class ClientService
     public List<Client> GetClientsByFilter(string? name, string? phoneNumber, string? passportDetails,
         DateTime? birthDateFrom, DateTime? birthDateTo)
     {
-        return _clientStorage.GetClients().Where(client =>
-            (string.IsNullOrEmpty(name) || client.Name.Contains(name) || client.Surname.Contains(name)) &&
-            (string.IsNullOrEmpty(phoneNumber) || client.PhoneNumber == phoneNumber) &&
-            (string.IsNullOrEmpty(passportDetails) || client.PassportDetails == passportDetails) &&
-            (!birthDateFrom.HasValue || client.BirthDate >= birthDateFrom) &&
-            (!birthDateTo.HasValue || client.BirthDate <= birthDateTo)
-        ).ToList();
-    }
-
-    public Client GetYoungestClient()
-    {
-        return _clientStorage.GetClients().OrderByDescending(client => client.BirthDate).FirstOrDefault();
-    }
-
-    public Client GetOldestClient()
-    {
-        return _clientStorage.GetClients().OrderBy(client => client.BirthDate).FirstOrDefault();
-    }
-
-    public double CalculateAverageAge()
-    {
-        return _clientStorage.GetClients().Average(client => client.Age);
+        return _clientStorage.GetClientsByFilter(name, phoneNumber, passportDetails, birthDateFrom, birthDateTo);
     }
 }
